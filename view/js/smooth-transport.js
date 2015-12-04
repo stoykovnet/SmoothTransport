a4d2a9922e8a106b2dfd96aa5d27c26fc1cd04d7
Stefan Stoykov <Stoykovnet@gmail.com>
2015-12-04 18:46:27 +0100

    .htaccess edited to allow CC  server to accept Authorization Header. client's js code is refactored.

--- view/js/smooth-transport.js
+++ view/js/smooth-transport.js
@@ -1,312 +1,314 @@
-var logistician = null;
-
 window.onload = function () {
-    logistician = readCookie('logistician');
-    if (logistician) {
-        loadDashboard();
-    } else {
-        setUpLoginForm();
+    if (!UILogin_callback.tryLogin(Cookie.read('logistician'))) {
+        UILogin.setUp();
     }
 };
 
-function enque_style(name) {
-    if (!document.getElementById(name))
-    {
-        var head = document.getElementsByTagName('head')[0];
-        var link = document.createElement('link');
-        link.id = name;
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = 'view/css/' + name + '.css';
-        head.appendChild(link);
-    }
-}
+/**
+ * Static Class
+ */
+var Ajax = new function () {
 
-function xhrGetTemplatePart(fileName, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'view/' + fileName, true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            if (callback) {
-                callback(xhr.responseText);
-            }
-        } else {
-            callback(0);
+    this.parseJSON = function (string) {
+        try {
+            var object = JSON.parse(string);
+            return object;
+        } catch (exception) {
+            alert(exception + ':' + string);
+            return null;
         }
     };
-    xhr.send();
-}
 
-function writeCookie(key, value) {
-    var valueEncoded = escape(JSON.stringify(value));
-    document.cookie = key + '=' + valueEncoded + ';';
-}
-
-function readCookie(key) {
-    var allValues = document.cookie;
-    var value = null;
-
-    if (allValues.toString().length > 0) {
-        allValues = allValues.split(';');
-
-        var i = 0, isFound = false;
-        while (i < allValues.length && !isFound) {
-            if (key === allValues[i].split('=')[0]) {
-                value = unescape((allValues[i].split('=')[1]));
-                try {
-                    value = JSON.parse(value);
-                } catch (exception) {
-                    alert(value);
-                }
-                isFound = true;
-            }
-        }
-    }
-    return value;
-}
-
-function deleteCookie(key) {
-    document.cookie = key +
-            '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
-}
-
-function setUpLoginForm() {
-    document.getElementById('username').addEventListener('keyup', function (event) {
-        if (event.keyCode === 13) {
-            document.getElementById('password').focus();
-        }
-    });
-
-    document.getElementById('password').addEventListener('keyup', function (event) {
-        if (event.keyCode === 13) {
-            loginLink_clicked();
-        }
-    });
-
-    document.getElementsByClassName('contents')[0].style.display = 'block';
-    document.getElementById('login-link').addEventListener('click', function () {
-        loginLink_clicked();
-    });
-}
-
-function loginLink_clicked() {
-    var form = document.getElementById('login-form');
-
-    if (form.username.value.trim().length && form.password.value.trim().length) {
+    this.getData = function (url, authorization, callback) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'api/v1/logistician/', true);
-        xhr.setRequestHeader('Authorization', form.username.value.trim() + ':' +
-                Sha256.hash(form.password.value.trim()));
+
+        xhr.open('GET', url, true);
+        if (authorization) {
+            xhr.setRequestHeader('Authorization', authorization);
+        }
 
         xhr.onload = function () {
             if (xhr.status === 200) {
-                try {
-                    logistician = JSON.parse(xhr.responseText);
-
-                    if (logistician !== null) {
-                        writeCookie('logistician', logistician);
-                        loadDashboard();
-                    } else {
-                        document.getElementById('auth-fail-message').style.display = 'block';
-                    }
-                } catch (e) {
-                    alert(xhr.responseText);
-                }
+                callback(xhr.responseText);
             } else {
-                alert('fail');
+                alert('Fail! ' + xhr.status + ':' + xhr.statusText);
+                callback(null);
             }
         };
+
         xhr.send();
+    };
+
+    this.getFile = function (fileName, callback) {
+        this.getData('view/' + fileName, false, callback);
+    };
+};
+// Ajax Class ends.
+
+/**
+ * Static Class
+ */
+var Cookie = new function () {
+
+    this.save = function (key, value) {
+        var valueEncoded = escape(JSON.stringify(value));
+        document.cookie = key + '=' + valueEncoded + ';';
+    };
+
+    this.read = function (key) {
+        var cookies = document.cookie;
+        if (cookies.indexOf(';')) {
+            var cookiesArray = cookies.split(';');
+            return Ajax.parseJSON(_find(key, cookiesArray));
+        }
+
+        return null;
+    };
+
+    function _find(key, cookies) {
+        for (var i = 0; i < cookies.length; i++) {
+            if (key === cookies[i].split('=')[0]) {
+                return unescape(cookies[i].split('=')[1]);
+            }
+        }
+
+        return null;
     }
-}
 
-function loadDashboard() {
-    xhrGetTemplatePart('dashboard.html', function (dash) {
-        enque_style('dashboard');
-        document.body.innerHTML = dash;
+    this.delete = function (key) {
+        document.cookie = key + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
+    };
+};
+// Cookie class ends.
 
-        document.getElementById('hello-name').innerHTML = logistician.first_name;
-        document.title = logistician.username.charAt(0).toUpperCase()
-                + logistician.username.slice(1) + ' : Dashboard';
+/**
+ * Static Class
+ */
+var UI = new function () {
 
-        showUnseenSMSCount();
-        loadUnseenSMS();
+    this.showElement = function (id, displayType) {
+        var element = document.getElementById(id);
+        element.style.display = displayType;
+    };
 
-        document.getElementById('new-link').addEventListener('click', function () {
-            loadUnseenSMS();
-        });
+    this.hideElement = function (id) {
+        var element = document.getElementById(id);
+        element.style.display = 'none';
+    };
 
-        document.getElementById('logout-link').addEventListener('click', function () {
-            deleteCookie('logistician');
-            location.reload();
-        });
-    });
-}
+    this.enqueStyle = function (name) {
+        if (!document.getElementById(name))
+        {
+            var link = document.createElement('link');
+            link.id = name;
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = 'view/css/' + name + '.css';
 
-function showUnseenSMSCount() {
-    xhrGetUnseenSMSCount(function (count) {
-        var counter = document.getElementById('unseen-counter');
-        if (count > 0) {
-            counter.innerHTML = ' (' + count + ')';
-            counter.style.display = 'initial';
-        } else {
-            counter.style.display = 'none';
-        }
-
-        setTimeout(showUnseenSMSCount, 30000);
-    });
-}
-
-function xhrGetUnseenSMSCount(callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost/smoothTransport/api/v1/logistician/' +
-            logistician.id + '/sms/unseen/count/', true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            if (callback) {
-                callback(xhr.responseText);
-                //alert(xhr.responseText);
-            }
-        } else {
-            callback(0);
-            //alert('Ajax error #2 occurred.');
+            document.getElementsByTagName('head')[0].appendChild(link);
         }
     };
-    xhr.send();
-}
 
-function xhrGetUnseenSMS(callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost/smoothTransport/api/v1/logistician/' +
-            logistician.id + '/sms/unseen/', true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            if (callback) {
-                callback(xhr.responseText);
-                alert(xhr.responseText);
-            }
-        } else {
-            callback(0);
-            //alert('Ajax error #2 occurred.');
+    this.validateInputValue = function (value) {
+        if (value.trim().length > 2) {
+            return true;
         }
+        return false;
     };
-    xhr.send();
-}
 
-function loadUnseenSMS() {
-    xhrGetUnseenSMS(function (messages) {
-        if (messages) {
-            var contents = document.getElementById('dashboard-contents');
-            contents.style.display = 'block';
-            try {
-                messages = JSON.parse(messages);
-                contents.innerHTML = '';
-                for (var i = 0; i < messages.length; i++) {
-                    xhrGetTruckData(i, messages[i].sender_id.id, function (x) {
-                    });
-                    contents.appendChild(createSMSElement(i, messages[i]));
+    this.setUpLink = function (id, invoke) {
+        var link = document.getElementById(id);
+        link.addEventListener('click', invoke);
+    };
+
+    this.changePageTitle = function (title) {
+        document.title = title;
+    };
+
+    this.capitalize = function (word) {
+        word = word.toLowerCase();
+        var capitalized = word.charAt(0).toUpperCase();
+        var otherChars = word.slice(1);
+        return capitalized + otherChars;
+    };
+};
+// UI class ends.
+
+/**
+ * Static Class
+ */
+var UILogin = new function () {
+
+    this.setUp = function () {
+        UI.showElement('login', 'block');
+        _setInputsBehavior();
+        UI.setUpLink('login-link', UILogin_callback.loginLink_clicked);
+    };
+
+    function _setInputsBehavior() {
+        var credentials = document.getElementsByClassName('credential');
+        credentials[0].addEventListener('keyup', function (event) {
+            (event.keyCode === 13) ? credentials[1].focus() : null;
+        });
+        credentials[1].addEventListener('keyup', function (event) {
+            (event.keyCode === 13) ? UILogin_callback.loginLink_clicked() : null;
+        });
+    }
+};
+// UILogin class ends.
+
+/**
+ * Static Class
+ */
+var UILogin_callback = new function () {
+
+    this.loggedLogistician = null;
+
+    this.loginLink_clicked = function () {
+        UI.hideElement('fail-message');
+
+        var login = document.getElementById('login-form');
+        var username = login.username.value, password = login.password.value;
+
+        if (UI.validateInputValue(username) && UI.validateInputValue(password)) {
+            _authenticateUser(username, password, function (logistician) {
+                if (!_tryLogin(logistician)) {
+                    UI.showElement('fail-message', 'block');
                 }
-            } catch (e) {
-                alert(messages);
-            }
-        }
-    });
-}
-
-function xhrGetTruckData(i, truckDriverId, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost/smoothTransport/api/v1/truck_driver/'
-            + truckDriverId
-            + '/truck', true);
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            if (callback) {
-                callback(xhr.responseText);
-                //alert(xhr.responseText);
-            }
+            });
         } else {
-            callback(0);
-            alert(i + ': ' + xhr.responseText);
+            UI.showElement('fail-message', 'block');
         }
     };
-    xhr.send();
-}
 
-function loadTruckData() {
+    function _authenticateUser(username, password, callback) {
+        var credentials = username + ':' + Sha256.hash(password);
+        Ajax.getData('api/v1/logistician/', credentials, function (logistician) {
+            callback(Ajax.parseJSON(logistician));
+        });
+    }
 
-}
+    function _tryLogin(logistician) {
+        if (logistician) {
+            _saveLogistician(logistician);
+            UIDashboard.setUp();
+            return true;
+        }
+        return false;
+    }
 
-function createSMSElement(id, data) {
-    var sms = document.createElement('div');
-    sms.setAttribute('id', 'sms-' + id);
-    sms.setAttribute('class', 'sms');
+    this.tryLogin = function (logistician) {
+        return _tryLogin(logistician);
+    };
 
-    var messageColumn = document.createElement('div');
-    messageColumn.setAttribute('class', 'sms-column');
-    messageColumn.innerHTML =
-            '<ul><li>' + id + '</li>'
-            + '<li class="sms-meta-data">'
-            + 'Stefan Stoykov'
-            + '<a href="tel:' + id + '">'
-            + '(' + id + ') ' + id + ' ' + id
-            + '<span></span></a></li>'
-            + '<li class="sms-meta-data">'
-            + 'Received On: ' + '2015-12-02 11:35'
-            + ' </li></ul>';
+    function _saveLogistician(logistician) {
+        if (!Cookie.read('logistician')) {
+            Cookie.save('logistician', logistician);
+        }
+        UILogin_callback.loggedLogistician = logistician;
+    }
+};
+// UILogin_callback class ends.
 
-    var truckData = document.createElement('div');
-    truckData.setAttribute('class', 'sms-column');
-    truckData.innerHTML =
-            '<ul><li>'
-            + 'Drives: ' + 'Scania' + '"PC 00 000"'
-            + '</li><li>'
-            + 'Was last at: ' + 'Copenhagen'
-            + '</li><li>'
-            + 'Heading to: ' + 'Aalborg'
-            + '</li></ul>';
+/**
+ * Static Class
+ */
+var UIDashboard = new function () {
 
-    var clear = document.createElement('div');
-    clear.setAttribute('class', 'clear');
+    this.setUp = function () {
+        _load();
+    };
 
-    sms.appendChild(messageColumn);
-    sms.appendChild(truckData);
-    sms.appendChild(createSMSControlLinks(id));
-    sms.appendChild(clear);
-    return sms;
-}
+    function _load() {
+        Ajax.getFile('dashboard.html', function (dashboard) {
+            UI.enqueStyle('dashboard');
+            document.body.innerHTML = dashboard;
 
-function createSMSControlLinks(id) {
-    var controls = document.createElement('div');
-    controls.setAttribute('class', 'sms-column last');
+            _updatePageTitle();
+            _setUpGreeting();
+            _setUpLinks();
 
-    var carriesLink = document.createElement('a');
-    carriesLink.setAttribute('title', 'See what is carrying.');
-    carriesLink.setAttribute('class', 'sms-control-link');
-    carriesLink.setAttribute('id', 'carries-link-' + id);
-    carriesLink.setAttribute('href', 'javascript:void(0);');
-    carriesLink.innerHTML =
-            '<div><img class="img-normal" src="view/img/carries_black_1.png" alt="Carries">'
-            + '<img class="img-hover" src="view/img/carries_white_1.png" alt="Carries">'
-            + '</div>';
-    carriesLink.addEventListener('click', function () {
-        alert(this.id);
-    });
+            UIInbox.setUp();
+        });
+    }
 
-    var resolveLink = document.createElement('a');
-    resolveLink.setAttribute('title', 'Resolve the issue');
-    resolveLink.setAttribute('class', 'sms-control-link');
-    resolveLink.setAttribute('id', 'resolve-link-' + id);
-    resolveLink.setAttribute('href', 'javascript:void(0);');
-    resolveLink.innerHTML =
-            '<div><img class="img-normal" src="view/img/resolve_black_1.png" alt="Resolve">'
-            + '<img class="img-hover" src="view/img/resolve_white_1.png" alt="Resolve">'
-            + '</div>';
-    resolveLink.addEventListener('click', function () {
-        alert(this.id);
-    });
+    function _updatePageTitle() {
+        var username = UILogin_callback.loggedLogistician.username;
+        UI.changePageTitle(UI.capitalize(username) + ' :: Dashboard');
+    }
 
-    controls.appendChild(carriesLink);
-    controls.appendChild(resolveLink);
+    function _setUpGreeting() {
+        var greeting = document.getElementById('greet-name');
+        greeting.innerHTML = UILogin_callback.loggedLogistician.first_name;
+    }
 
-    return controls;
-}
+    function _setUpLinks() {
+        UI.setUpLink('logout-link', UIDashboard_callback.logoutLink_clicked);
+        UI.setUpLink('inbox-link', UIDashboard_callback.inboxLink_clicked);
+    }
+};
+// UIDashboard class ends.
+
+/**
+ * Static Class
+ */
+var UIDashboard_callback = new function () {
+
+    this.inboxLink_clicked = function () {
+        UIInbox.setUp();
+    };
+
+    this.logoutLink_clicked = function () {
+        Cookie.delete('logistician');
+        location.reload();
+    };
+};
+// UIDashboard_callback class ends.
+
+/**
+ * Static Class
+ */
+var UIInbox = new function () {
+
+    this.setUp = function () {
+        UIInbox_callback.autoCountUnseenMessages();
+        _loadMessages();
+    };
+
+    this.updateUnseenCounter = function (number) {
+        var counter = document.getElementById('unseen-counter');
+        UI.hideElement(counter.id);
+
+        if (number) {
+            counter.innerHTML = ' (' + number + ')';
+            UI.showElement(counter.id, 'inline-block');
+        }
+    };
+
+    function _loadMessages() {
+        alert('No messages');
+        return null;
+    }
+};
+//UIInbox class ends.
+
+/**
+ * Static Class
+ */
+var UIInbox_callback = new function () {
+
+    this.autoCountUnseenMessages = function () {
+        _countUnseenMessages(function (total) {
+            UIInbox.updateUnseenCounter(total);
+        });
+        if (UILogin_callback.loggedLogistician) {
+            setTimeout(this.autoCountUnseenMessages, 120 * 1000);
+        }
+    };
+
+    function _countUnseenMessages(callback) {
+        var id = UILogin_callback.loggedLogistician.id;
+        var url = 'api/v1/logistician/' + id + '/sms/unseen/count/';
+        Ajax.getData(url, false, callback);
+    }
+};
